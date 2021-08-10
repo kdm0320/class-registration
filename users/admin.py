9a1cbc7363bcb62b727c194bd3b7253278fc2334
Dogmin <kdm3578@gmail.com>
2021-08-10 15:47:37 +0900

    first set user data field

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.User)  # decorator
+class CustomUserAdmin(UserAdmin):
+    """Custom User Admin"""
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Personal information",
+            {
+                "fields": (
+                    "number",
+                    "major",
+                    "grade",
+                    "basket_subjects",
+                    "registed_subjects",
+                    "time_table",
+                )
+            },
+        ),
+    )
+    list_display = (
+        "username",
+        "number",
+        "major",
+        "grade",
+        "email",
+    )
+    list_filter = ("major", "grade")
--- users/models.py
+++ users/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+# class IntegerRangeField(models.IntegerField):
+#     def __init__(
+#         self, verbose_name=None, name=None, min_value=None, max_value=None, **kwargs
+#     ):
+#         self.min_value, self.max_value = min_value, max_value
+#         models.IntegerField.__init__(self, verbose_name, name, **kwargs)
+
+#     def formfield(self, **kwargs):
+#         defaults = {"min_value": self.min_value, "max_value": self.max_value}
+#         defaults.update(kwargs)
+#         return super(IntegerRangeField, self).formfield(**defaults)
+
+
+class User(AbstractUser):
+    """Custom User Model"""
+
+    GRADE_FIRST = "1"
+    GRADE_SECOND = "2"
+    GRADE_THIRD = "3"
+    GRADE_FOURTH = "4"
+
+    GRADE_CHOICES = (
+        (GRADE_FIRST, "1"),
+        (GRADE_SECOND, "2"),
+        (GRADE_THIRD, "3"),
+        (GRADE_FOURTH, "4"),
+    )
+    number = models.PositiveIntegerField(
+        default=0, validators=[MaxValueValidator(99999999)]
+    )
+    major = models.CharField(null=True, max_length=15)
+    grade = models.CharField(choices=GRADE_CHOICES, max_length=2, null=True)
+    basket_subjects = models.TextField(blank=True)
+    registed_subjects = models.TextField(blank=True)
+    time_table = models.TextField(blank=True)
