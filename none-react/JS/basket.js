0d94622c1ca9671bd116353048924c3aecb7599b
A1pe <cows0511@gmail.com>
2021-08-18 14:11:17 +0900

    added enrol page logic
    added enrol page logic and connected link to basket.html

--- none-react/JS/basket.js
+++ none-react/JS/basket.js
@@ -1,6 +1,8 @@
-const btnContentInfo = document.querySelector(".content_index_info");
-const btnContentList = document.querySelector(".content_index_list");
-const btnContentSchedule = document.querySelector(".content_index_schedule");
+const btnBasketContentInfo = document.querySelector(".content_index_info");
+const btnBasketContentList = document.querySelector(".content_index_list");
+const btnBasketContentSchedule = document.querySelector(
+  ".content_index_schedule"
+);
 
 const contentBasketInfo = document.querySelector(".content_basket_info");
 const contentBaskteList = document.querySelector(".content_basket_list");
@@ -8,64 +10,67 @@ const contentBaskteSchedule = document.querySelector(
   ".content_basket_schedule"
 );
 
-let isShow = true;
+let isBasketShow = true;
 
-const infoToggle = function () {
+const basketInfoToggle = function () {
   if (contentBasketInfo.style.display === "none") {
-    if (isShow === false && contentBasketInfo.style.display === "none")
-      isShow = true;
-    contentBasketInfo.style.display = isShow ? "block" : "none";
+    if (isBasketShow === false && contentBasketInfo.style.display === "none")
+      isBasketShow = true;
+    contentBasketInfo.style.display = isBasketShow ? "block" : "none";
     contentBaskteSchedule.style.display = "none";
     contentBaskteList.style.display = "none";
-    isShow = !isShow;
+    isBasketShow = !isBasketShow;
     // 수강신청 안내 페이지가 표시되지 않았을 때 클릭 시 수강신청 안내 페이지를 표시함
   }
 };
 
-const listToggle = function () {
+const basketlistToggle = function () {
   if (
     contentBasketInfo.style.display === "none" &&
     contentBaskteList.style.display === "block"
   ) {
-    isShow = true;
-    contentBasketInfo.style.display = isShow ? "block" : "none";
+    isBasketShow = true;
+    contentBasketInfo.style.display = isBasketShow ? "block" : "none";
     contentBaskteSchedule.style.display = "none";
     contentBaskteList.style.display = "none";
-    isShow = !isShow;
+    isBasketShow = !isBasketShow;
     // 장바구니 목록이 보인 상태에서 클릭 시 수강신청 안내창이 보이도록 설정
   } else {
-    if (isShow === false && contentBaskteList.style.display === "none")
-      isShow = true;
-    contentBaskteList.style.display = isShow ? "block" : "none";
+    if (isBasketShow === false && contentBaskteList.style.display === "none")
+      isBasketShow = true;
+    contentBaskteList.style.display = isBasketShow ? "block" : "none";
     contentBaskteSchedule.style.display = "none";
     contentBasketInfo.style.display = "none";
-    isShow = !isShow;
+    isBasketShow = !isBasketShow;
     // 수강신청 및 장바구니 시간표가 보인 상태라면 장바구니 목록을 보이도록 설정
   }
 };
 
-const scheduleToggle = function () {
+const basketScheduleToggle = function () {
   if (
     contentBasketInfo.style.display === "none" &&
     contentBaskteSchedule.style.display === "block"
   ) {
-    isShow = true;
-    contentBasketInfo.style.display = isShow ? "block" : "none";
+    isBasketShow = true;
+    contentBasketInfo.style.display = isBasketShow ? "block" : "none";
     contentBaskteSchedule.style.display = "none";
     contentBaskteList.style.display = "none";
-    isShow = !isShow;
+    isBasketShow = !isBasketShow;
     // 장바구니 시간표가 보인 상태에서 클릭 시 수강신청 안내창이 보이도록 설정
   } else {
-    if (isShow === false && contentBaskteSchedule.style.display === "none")
-      isShow = true;
-    contentBaskteSchedule.style.display = isShow ? "block" : "none";
+    if (
+      isBasketShow === false &&
+      contentBaskteSchedule.style.display === "none"
+    )
+      isBasketShow = true;
+    contentBaskteSchedule.style.display = isBasketShow ? "block" : "none";
     contentBaskteList.style.display = "none";
     contentBasketInfo.style.display = "none";
-    isShow = !isShow;
+    isBasketShow = !isBasketShow;
     // 수강신청 및 장바구니 목록이 보인 상태라면 시간표를 보이도록 설정.
   }
 };
 
-btnContentInfo.onclick = infoToggle;
-btnContentList.onclick = listToggle;
-btnContentSchedule.onclick = scheduleToggle;
+btnBasketContentInfo.onclick = basketInfoToggle;
+btnBasketContentList.onclick = basketlistToggle;
+btnBasketContentSchedule.onclick = basketScheduleToggle;
