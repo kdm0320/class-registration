f233051bfa56a14404f5a05320afbe1c9c22485e
Dogmin <kdm3578@gmail.com>
2021-08-24 18:04:22 +0900

    name,grade,number add
    이름,학년,학번 html과 동기화

--- users/views.py
+++ users/views.py
@@ -3,10 +3,9 @@ from django.shortcuts import redirect, render
 from django.urls.base import reverse
 from django.views import View
 from django.views.generic import FormView
-
 from . import forms
+from . import models
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 class LoginView(FormView):
@@ -22,39 +21,17 @@ class LoginView(FormView):
             user = authenticate(request, username=id, password=password)
             if user is not None:
                 login(request, user)
-                return redirect("users:enrollment")
+                return redirect(reverse("users:enrollment"))
 
         return render(request, "index.html", {"form": form})
 
 
-# def index(request):
-#     if request.COOKIES.get("username") is not None:
-#         response = render(request, "index.html")
-#         response.delete_cookie("username")
-#         response.delete_cookie("password")
-#         logout(request)
-#         return response
-
-
-# def log_in(request):
-#     if request.user.is_authenticated:
-#         return redirect("users:enrollment")
-
-#     if request.method == "POST":
-#         form = AuthenticationForm(request, request.POST)
-#         if form.is_valid():
-#             login(request, form.get_user())
-#             return redirect("core:index")
-#     else:
-#         form = AuthenticationForm()
-#     return render(request, "index.html", {"form": form})
-
-
 def log_out(request):
-
     logout(request)
+    return redirect(reverse("core:index"))
 
-    return redirect("core:index")
+
+ERROR_PAGE = "core:error"
 
 
 def enrollment(request):
@@ -64,7 +41,7 @@ def enrollment(request):
             request, template_name, {"is_login": request.session["_auth_user_id"]}
         )
     except KeyError:
-        return redirect("core:error")
+        return redirect(reverse(ERROR_PAGE))
 
 
 def basket(request):
@@ -74,7 +51,7 @@ def basket(request):
             request, template_name, {"is_login": request.session["_auth_user_id"]}
         )
     except KeyError:
-        return redirect("core:error")
+        return redirect(reverse(ERROR_PAGE))
 
 
 def user_schedule(request):
@@ -85,4 +62,4 @@ def user_schedule(request):
             request, template_name, {"is_login": request.session["_auth_user_id"]}
         )
     except KeyError:
-        return redirect("core:error")
+        return redirect(reverse(ERROR_PAGE))
