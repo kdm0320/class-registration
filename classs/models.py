93f5f50e2f92751de2b61e3594ae27fc33a37404
Dogmin <kdm3578@gmail.com>
2021-08-29 23:33:06 +0900

    class model setting
    강좌 모델 설정

--- classs/models.py
+++ classs/models.py
@@ -5,7 +5,17 @@ from core import models as core_models
 class Class(core_models.ClassInfoModel):
     """Class Model Definition"""
 
-    department = models.CharField(("개설학과"), max_length=20, blank=True)
+    DEP_1 = "경제학과"
+    DEP_2 = "전자공학과"
+    DEP_3 = "소프트웨어학과"
+    DEP_CHOICES = (
+        (DEP_1, "경제학과"),
+        (DEP_2, "전자공학과"),
+        (DEP_3, "소프트웨어학과"),
+    )
+    department = models.CharField(
+        ("개설학과"), choices=DEP_CHOICES, max_length=20, default="--선택--"
+    )
 
     def __str__(self):
         return self.subject_name
