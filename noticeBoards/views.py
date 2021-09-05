91d4a98094553a2b224fc4ebc3dbd2f264c58490
Dogmin <kdm3578@gmail.com>
2021-09-05 14:46:33 +0900

    FBV(add Exception)

--- noticeBoards/views.py
+++ noticeBoards/views.py
@@ -1,18 +1,27 @@
 from noticeBoards.models import notice
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator
-from . import models
-import math
+from django.core.paginator import EmptyPage, Paginator
 
 
 @login_required(login_url="/login/")
 def home(request):
     template_name = "noticeBoards/notice.html"
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)
     datas = notice.objects.all()
-    paginator = Paginator(datas, 3)
-    notice_page = paginator.get_page(page)
+    paginator = Paginator(datas, 3, orphans=1)
+    try:
+        notice_page = paginator.page(int(page))
+        return render(
+            request,
+            template_name,
+            {
+                "notices": notice_page,
+            },
+        )
+    except EmptyPage:
+        return redirect("notices:board")
+
     if request.method == "POST":
         notice.objects.create(
             title=request.POST["title"],
