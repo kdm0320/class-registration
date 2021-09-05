8b1afae885a6eac5d351655bab49a5046a0910bd
Dogmin <kdm3578@gmail.com>
2021-09-05 15:39:36 +0900

    notice view upadate(CBV)

--- noticeBoards/urls.py
+++ noticeBoards/urls.py
@@ -4,6 +4,6 @@ from . import views as board_views
 app_name = "notices"
 
 urlpatterns = [
-    path("", board_views.home, name="board"),
+    path("", board_views.noticeView.as_view(), name="board"),
     path("", board_views.notice_detail, name="detail"),
 ]
--- noticeBoards/views.py
+++ noticeBoards/views.py
@@ -1,28 +1,18 @@
 from noticeBoards.models import notice
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import EmptyPage, Paginator
+from django.views.generic import ListView
 
 
-@login_required(login_url="/login/")
-def home(request):
-    template_name = "noticeBoards/notice.html"
-    page = request.GET.get("page", 1)
-    datas = notice.objects.all()
-    paginator = Paginator(datas, 3, orphans=1)
-    try:
-        notice_page = paginator.page(int(page))
-        return render(
-            request,
-            template_name,
-            {
-                "notices": notice_page,
-            },
-        )
-    except EmptyPage:
-        return redirect("notices:board")
+# @login_required(login_url="/login/")
+class noticeView(ListView):
+    """noticeView Definition"""
 
-    if request.method == "POST":
+    model = notice
+    paginate_by = 3
+    ordering = "created"
+
+    def post(self, request):
         notice.objects.create(
             title=request.POST["title"],
             type=request.POST["type"],
@@ -31,13 +21,34 @@ def home(request):
             writer=request.POST["writer"],
         )
 
-    return render(
-        request,
-        template_name,
-        {
-            "notices": notice_page,
-        },
-    )
+        return redirect("notices:board")
+
+
+# def home(request):
+#     template_name = "noticeBoards/notice.html"
+#     page = request.GET.get("page", 1)
+#     datas = notice.objects.all()
+#     paginator = Paginator(datas, 3, orphans=1)
+
+#     if request.method == "POST":
+#         notice.objects.create(
+#             title=request.POST["title"],
+#             type=request.POST["type"],
+#             content=request.POST["content"],
+#             department=request.POST["department"],
+#             writer=request.POST["writer"],
+#         )
+#     try:
+#         notice_page = paginator.page(int(page))
+#         return render(
+#             request,
+#             template_name,
+#             {
+#                 "notices": notice_page,
+#             },
+#         )
+#     except EmptyPage:
+#         return redirect("notices:board")
 
 
 def notice_detail(request):
