8f9aaccbd6751f23b3e4f8db06a785b553671872
Dogmin <kdm3578@gmail.com>
2021-09-08 18:03:28 +0900

    update noticeDetail.html and search

--- noticeBoards/views.py
+++ noticeBoards/views.py
@@ -1,11 +1,10 @@
-from django.http.response import Http404
-from noticeBoards import models
-from noticeBoards.models import notice
-from django.shortcuts import redirect, render
-from django.views.generic import ListView, DetailView
-from django.core.paginator import Paginator
-
+# from django.http.response import Http404
+# from noticeBoards import models
+# from django.core.paginator import Paginator
 # from django.contrib.auth.decorators import login_required
+from noticeBoards.models import notice
+from django.shortcuts import redirect
+from django.views.generic import ListView, DetailView
 
 
 class NoticeView(ListView):
@@ -97,33 +96,47 @@ class NoticeDetail(DetailView):
 
 class SeachView(ListView):
     template_name = "noticeBoards/notice_search.html"
-    model = notice
     paginate_by = 3
     ordering = "created"
     context_object_name = "target"
-    title_list = ""
 
     def get_queryset(self, **kwargs):
         title = self.request.GET.get("search")
         if title is not None:
-            self.title_list = title
+            global title_list
+            title_list = title
             filter_args = {"title__startswith": title}
             target_notices = notice.objects.filter(**filter_args)
             return target_notices
         else:
-            filter_args = {"title__startswith": self.title_list}
+            filter_args = {"title__startswith": title_list}
             target_notices = notice.objects.filter(**filter_args)
             return target_notices
 
 
-def search(request):
-    template_name = "noticeBoards/notice_search.html"
-    title = request.GET.get("search", "제목을 입력하세요")
-    filter_args = {"title__startswith": title}
-    target_notices = notice.objects.filter(**filter_args)
-    page = request.GET.get("page", 1)
-    paginator = Paginator(target_notices, 3, orphans=1)
-    notice_page = paginator.get_page(page)
-    return render(
-        request, template_name, {"target": notice_page, "search_title": title}
-    )
+title_list = ""
+
+
+# def search(request):
+#     template_name = "noticeBoards/notice_search.html"
+#     title = request.GET.get("search")
+#     page = request.GET.get("page", 1)
+#     title_list = title
+#     if title is not None:
+#         title_list = title
+#         print(title_list)
+#         filter_args = {"title__startswith": title}
+#         target_notices = notice.objects.filter(**filter_args)
+#         paginator = Paginator(target_notices, 3)
+#         notice_page = paginator.get_page(page)
+#         return render(
+#             request, template_name, {"target": notice_page, "search_title": title}
+#         )
+#     else:
+#         filter_args = {"title__startswith": title_list}
+#         target_notices = notice.objects.filter(**filter_args)
+#         paginator = Paginator(target_notices, 3)
+#         notice_page = paginator.get_page(page)
+#         return render(
+#             request, template_name, {"target": notice_page, "search_title": title}
+#         )
---
+++ static/JS/noticeDetail.js
@@ -0,0 +1,27 @@
+const eventButton = document.querySelector("#fixing_button");
+const fixedInterface = document.querySelector("#notice_detail_fixed_button_interface");
+const cancleFixed = fixedInterface.querySelector("#cancle_button");
+const showedMaster = document.querySelector("#only_showed_notice_master");
+const detailWriter = document.querySelector("#detail_writer");
+const logedinUser = document.querySelector("#target_data");
+
+const writer = detailWriter.textContent;
+const target = logedinUser.textContent;
+
+fixedInterface.style.display = "none";
+showedMaster.style.display = "none";
+
+if ( writer == target) {
+    showedMaster.style.display = "block";
+}
+
+
+eventButton.addEventListener("click", () => {
+    fixedInterface.style.display = "block";
+    eventButton.style.display = "none";
+})
+
+cancleFixed.addEventListener("click", () => {
+    fixedInterface.style.display = "none";
+    eventButton.style.display = "block";
+})
