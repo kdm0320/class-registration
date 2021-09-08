aca86f52bf6590ac47f57fe0c7889097f322cfee
Eddy <46862196+A1pe@users.noreply.github.com>
2021-09-08 15:07:10 +0900

    updated files from back
    백단 구현 자료 업로드.

--- noticeBoards/urls.py
+++ noticeBoards/urls.py
@@ -4,6 +4,8 @@ from . import views as board_views
 app_name = "notices"
 
 urlpatterns = [
-    path("", board_views.noticeView.as_view(), name="board"),
-    path("notce/<int:pk>/", board_views.notice_detail, name="detail"),
+    path("", board_views.NoticeView.as_view(), name="board"),
+    path("myboard/", board_views.MyNoticeView.as_view(), name="find"),
+    path("notice/<int:pk>/", board_views.NoticeDetail.as_view(), name="detail"),
+    path("search/", board_views.SeachView.as_view(), name="search"),
 ]
--- noticeBoards/views.py
+++ noticeBoards/views.py
@@ -1,35 +1,15 @@
-#from noticeBoards.models import notice
-#from django.shortcuts import redirect, render
-#from django.urls.base import reverse
-#from django.core import serializers
-#from django.contrib.auth.decorators import login_required
-
-#@login_required(login_url="/login/")
-# def home(request):
-#    template_name = "noticeBoards/notice.html"
-#    notices = serializers.serialize(
-#        "json",
-#        notice.objects.all(),
-#        fields=(
-#            "title",
-#            "type",
-#            "content",
-#            "department",
-#            "writer",
-#            "created",
-#            "post_hit",
-#        ),
-#    )
-#    return render(request, template_name, {"notices": notices})
-
-
+from django.http.response import Http404
+from noticeBoards import models
 from noticeBoards.models import notice
 from django.shortcuts import redirect, render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.core.paginator import Paginator
+
+# from django.contrib.auth.decorators import login_required
 
 
-class noticeView(ListView):
-    """noticeView Definition"""
+class NoticeView(ListView):
+    """NoticeView Definition"""
 
     model = notice
     paginate_by = 3
@@ -48,5 +28,102 @@ class noticeView(ListView):
         return redirect("notices:board")
 
 
-def notice_detail(request, pk):
-    return render(request, "noticeBoards/noticeDetail.html")
+class MyNoticeView(ListView):
+    """MyNoticeView Definition"""
+
+    template_name = "noticeBoards/my_notice_list.html"
+    model = notice
+    paginate_by = 3
+    ordering = "created"
+    context_object_name = "notices"
+
+    def get_queryset(self, **kwargs):
+        user = self.request.user.last_name + self.request.user.first_name
+        user_notice = notice.objects.filter(writer=user)
+        return user_notice
+
+    def post(self, request):
+        notice.objects.create(
+            title=request.POST["title"],
+            type=request.POST["type"],
+            content=request.POST["content"],
+            department=request.POST["department"],
+            writer=request.POST["writer"],
+        )
+
+        return redirect("notices:board")
+
+
+# @login_required(login_url="/login/")
+# def home(request):
+#     template_name = "noticeBoards/notice_list.html"
+#     page = request.GET.get("page", 1)
+#     datas = notice.objects.all()
+#     paginator = Paginator(datas, 3, orphans=1)
+
+#     notice_page = paginator.get_page(page)
+#     if request.method == "POST":
+#         notice.objects.create(
+#             title=request.POST["title"],
+#             type=request.POST["type"],
+#             content=request.POST["content"],
+#             department=request.POST["department"],
+#             writer=request.POST["writer"],
+#         )
+#     return render(
+#         request,
+#         template_name,
+#         {
+#             "notices": notice_page,
+#         },
+#     )
+
+
+class NoticeDetail(DetailView):
+
+    """NoticeDetail Definition"""
+
+    model = notice
+
+
+# def notice_detail(request, pk):
+#     template_name = "noticeBoards/notice_detail.html"
+#     try:
+#         target_notice = notice.objects.get(pk=pk)
+#         return render(request, template_name, {"notice": target_notice})
+#     except notice.DoesNotExist:
+#         raise Http404()
+
+
+class SeachView(ListView):
+    template_name = "noticeBoards/notice_search.html"
+    model = notice
+    paginate_by = 3
+    ordering = "created"
+    context_object_name = "target"
+    title_list = ""
+
+    def get_queryset(self, **kwargs):
+        title = self.request.GET.get("search")
+        if title is not None:
+            self.title_list = title
+            filter_args = {"title__startswith": title}
+            target_notices = notice.objects.filter(**filter_args)
+            return target_notices
+        else:
+            filter_args = {"title__startswith": self.title_list}
+            target_notices = notice.objects.filter(**filter_args)
+            return target_notices
+
+
+def search(request):
+    template_name = "noticeBoards/notice_search.html"
+    title = request.GET.get("search", "제목을 입력하세요")
+    filter_args = {"title__startswith": title}
+    target_notices = notice.objects.filter(**filter_args)
+    page = request.GET.get("page", 1)
+    paginator = Paginator(target_notices, 3, orphans=1)
+    notice_page = paginator.get_page(page)
+    return render(
+        request, template_name, {"target": notice_page, "search_title": title}
+    )
