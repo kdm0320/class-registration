72753a78049c406084b692082aa49a19cb48292e
Dogmin <kdm3578@gmail.com>
2021-09-13 12:56:37 +0900

    other things

--- noticeBoards/forms.py
+++ noticeBoards/forms.py
@@ -1,11 +1,5 @@
 from django import forms
-from django.forms.widgets import Textarea
 
 
-class NoticForm(forms.Form):
-    title = forms.CharField(label="제목", min_length=3, max_length=20)
-    options = (("Notice", "공지"), ("Question", "문의"))
-    type = forms.ChoiceField(label="종류", choices=options)
-    content = forms.CharField(label="내용", widget=Textarea)
-    department = forms.CharField(label="소속", max_length=10)
-    writer = forms.CharField(label="작성자")
+class SearchForm(forms.Form):
+    title = forms.CharField(initial="Nothing")
