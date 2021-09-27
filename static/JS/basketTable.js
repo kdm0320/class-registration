b041476b26073cfb0190256608d6e6fac16ceb4a
Eddy <46862196+A1pe@users.noreply.github.com>
2021-09-27 14:31:37 +0900

    uploaded files
    basket 관련 데이터 업로드.
    ajax를 이용한 emrolment 쪽으로 데이터 전송 구현.
    제이쿼리에 의존하지 않는 코드 구현.

---
+++ static/JS/basketTable.js
@@ -0,0 +1,82 @@
+const basketData = document.querySelector("#basket_datas").textContent;
+const basketDataObj = JSON.parse(basketData);
+
+const basketTbody = document.querySelector("#target_basket_table");
+const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+const dataArray = ['grade','check_major','subject_number','subject_name','credit','professor','time','people'];
+
+for (let clazz in basketDataObj) {
+    let classTr = document.createElement("tr");
+    let button = document.createElement("button");
+    let buttonName = document.createTextNode("수강신청");
+    let datas = basketDataObj[clazz];
+    let arrayIndex = 0;
+
+    button.className = "btnAjax";
+    
+    button.appendChild(buttonName);
+    classTr.appendChild(button)
+    for (let data in datas) {
+        let targetData = datas[data];
+        let classTd = document.createElement("td");
+        let tdText = document.createTextNode(targetData);
+        switch (data) {
+            case 'id':
+                classTd.className = "hidden";
+                classTd.appendChild(tdText);
+                classTr.appendChild(classTd);
+                break;
+            case 'universe':
+                break;
+            case 'department':
+                break;
+            default:
+                classTd.className = dataArray[arrayIndex];
+                classTd.appendChild(tdText);
+                classTr.appendChild(classTd);
+                arrayIndex += 1;
+                break;
+
+        }
+    }
+    basketTbody.appendChild(classTr);
+
+    button.addEventListener('click', e => {
+        let id = classTr.querySelector('.hidden').innerText;
+        let grade = classTr.querySelector('.grade').innerText;
+        let check_major = classTr.querySelector('.check_major').innerText;
+        let subject_number = classTr.querySelector('.subject_number').innerText;
+        let subject_name = classTr.querySelector('.subject_name').innerText;
+        let credit = classTr.querySelector('.credit').innerText;
+        let time = classTr.querySelector('.time').innerText
+        let professor = classTr.querySelector('.professor').innerText;
+        let people = classTr.querySelector('.people').innerText;
+        let param = {
+          'id' : id,
+          'grade' : grade,
+          'check_major' : check_major,
+          'subject_number' : subject_number,
+          'subject_name' : subject_name,
+          'credit' : credit,
+            'professor': professor,
+            'time': time,
+            'people': people,
+          }
+        
+        const reqUrl = new Request("/users/enrollment", { headers: { 'X-CSRFToken': csrftoken } })
+        fetch(reqUrl, {
+            method: "POST",
+            body: JSON.stringify(param),
+            mode: 'same-origin'
+        }
+        ).then(function (res) {
+            return res.text()
+        }, function (error) {
+            console.log(error)
+        }
+        )
+        basketTbody.removeChild(classTr);
+
+      });
+
+}
