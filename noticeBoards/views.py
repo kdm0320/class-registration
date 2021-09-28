5070b7c4c6e3676fe4ad3cc61ae17cd2570032db
Dogmin <kdm3578@gmail.com>
2021-09-28 22:50:38 +0900

    Delete comment

--- noticeBoards/views.py
+++ noticeBoards/views.py
@@ -64,39 +64,6 @@ class MyNoticeView(ListView):
         return redirect("notices:board")
 
 
-# @login_required(login_url="/login/")
-# def home(request):
-#     template_name = "noticeBoards/notice_list.html"
-#     page = request.GET.get("page", 1)
-#     datas = notice.objects.all()
-#     paginator = Paginator(datas, 3, orphans=1)
-
-#     notice_page = paginator.get_page(page)
-#     if request.method == "POST":
-#         notice.objects.create(
-#             title=request.POST["title"],
-#             type=request.POST["type"],
-#             content=request.POST["content"],
-#             department=request.POST["department"],
-#             writer=request.POST["writer"],
-#         )
-#     return render(
-#         request,
-#         template_name,
-#         {
-#             "notices": notice_page,
-#         },
-#     )
-
-
-# class NoticeDetail(DetailView):
-
-#     """NoticeDetail Definition"""
-
-#     model = notice
-#     context_object_name = "notice"
-
-
 def notice_detail(request, pk):
     template_name = "noticeBoards/notice_detail.html"
     try:
@@ -125,30 +92,6 @@ class SeachView(ListView):
             filter_args = {"title__startswith": self.request.session["search_word"]}
             target_notices = notice.objects.filter(**filter_args)
             return target_notices
-
-
-# def search(request):
-#     template_name = "noticeBoards/notice_search.html"
-#     title = request.GET.get("search")
-#     page = request.GET.get("page", 1)
-#     if title is not None:
-#         global title_list
-#         title_list = title
-#         filter_args = {"title__startswith": title}
-#         target_notices = notice.objects.filter(**filter_args)
-#         paginator = Paginator(target_notices, 3)
-#         notice_page = paginator.get_page(page)
-#         return render(
-#             request, template_name, {"target": notice_page, "search_title": title}
-#         )
-#     else:
-#         filter_args = {"title__startswith": title_list}
-#         target_notices = notice.objects.filter(**filter_args)
-#         paginator = Paginator(target_notices, 3)
-#         notice_page = paginator.get_page(page)
-#         return render(
-#             request, template_name, {"target": notice_page, "search_title": title}
-#         )
 
 
 def save(request, pk):
