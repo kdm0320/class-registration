3f512802fb64da366ec985a546d5bce6661f99b9
Eddy <46862196+A1pe@users.noreply.github.com>
2021-10-02 12:33:14 +0900

    updated backend files
    백엔드 자료 업로드

--- registrations/views.py
+++ registrations/views.py
@@ -4,9 +4,10 @@ from django.core.serializers.json import DjangoJSONEncoder
 from django.http.response import JsonResponse
 from django.shortcuts import render
 from . import models
+from classs import models as class_model
 
 
-@login_required(login_url="/login/")
+@login_required(login_url="users/login/")
 def enrollment(request):
     template_name = "enrolment.html"
 
@@ -17,18 +18,17 @@ def enrollment(request):
         for i in range(len(data)):
             temp_data[f"class{i}"] = data[i]
         datas = json.dumps(temp_data, ensure_ascii=False, cls=DjangoJSONEncoder)
+
         return render(request, template_name, {"regi_datas": datas})
     else:
         return render(request, template_name)
-
-    
 
 
 def delete(request):
     jsonObject = json.loads(request.body)
     target_list = models.registration.objects.get(user=request.user).subjects
     target_pk = jsonObject.get("id")
-    target_name = models.registration.objects.get(pk=target_pk)
+    target_name = class_model.Class.objects.get(id=target_pk)
     target_list.remove(target_name)
     non_data = {}
     return JsonResponse(non_data)
