1a0e60d2df14882fb99912aea3202e6c53434018
Dogmin <kdm3578@gmail.com>
2021-10-09 14:34:21 +0900

    Update from Front

--- static/JS/basketTable.js
+++ static/JS/basketTable.js
@@ -31,7 +31,7 @@ for (let clazz in basketDataObj) {
                 break;
             case 'department':
                 break;
-            case 'subject_name':
+            /*case 'subject_name':
                 classTd.className = dataArray[arrayIndex];
                 classTd.appendChild(tdText);
                 classTd.addEventListener('click', e => {
@@ -40,7 +40,7 @@ for (let clazz in basketDataObj) {
                 })
                 classTr.appendChild(classTd);
                 arrayIndex += 1;
-                break;
+                break;*/
             default:
                 classTd.className = dataArray[arrayIndex];
                 classTd.appendChild(tdText);
@@ -90,9 +90,3 @@ for (let clazz in basketDataObj) {
         basketTbody.removeChild(classTr);
     });
 }
-//let options = {
-//    width: "60rem",
-//    height:"60rem"
-//}
-//PDFObject.embed("../static/pdf/noname_1.pdf", "#example1", options);
-//console.log(PDFObject)
