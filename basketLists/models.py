0260864efdaca8573bc2bec1b4f5dd1ed1218e9a
Dogmin <kdm3578@gmail.com>
2021-10-14 15:43:08 +0900

    Update Data structure
    서버 실행전 혹은 서버를 끄고
     python manage.py makemigrations -> python manage.py migrate
    
    -> admin 페이지에 들어가서 BASKETLISTS -> Lists에서 본인 아이디 삭제 그리고 다시 서버 실행

--- basketLists/models.py
+++ basketLists/models.py
@@ -9,7 +9,9 @@ class List(models.Model):
     )
     subjects = models.ManyToManyField("classs.Class", blank=True)
     objects = core_managers.CustomModelManager()
-    time_table = models.JSONField(("시간표"), null=True, blank=True)
+    time_table = models.JSONField(
+        ("시간표"), null=True, default=dict(월=[], 화=[], 수=[], 목=[], 금=[])
+    )
 
     def number(self):
         return self.user
