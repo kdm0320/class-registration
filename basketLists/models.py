8cd4fde6e017cdb5e5477605821aa4397880e8a7
Dogmin <kdm3578@gmail.com>
2021-10-24 12:09:24 +0900

    Update Data structure 서버 실행전 혹은 서버를 끄고  python manage.py makemigrations -> python manage.py migrate

--- basketLists/models.py
+++ basketLists/models.py
@@ -1,10 +1,23 @@
-from registrations import models as regist_model
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from core import managers as core_managers
 
 
 def default_time_table_dict():
     return {"월": [], "화": [], "수": [], "목": [], "금": []}
 
 
-class List(regist_model.registration):
-    class Meta:
-        proxy = True
+class List(models.Model):
+
+    user = models.OneToOneField(
+        "users.User", related_name="lists", on_delete=models.CASCADE, null=True
+    )
+    subjects = models.ManyToManyField("classs.Class", blank=True)
+    objects = core_managers.CustomModelManager()
+    time_table = models.JSONField(("시간표"), null=True, default=default_time_table_dict)
+    credits = models.FloatField(
+        ("신청학점"), default=0, validators=[MinValueValidator(0), MaxValueValidator(23)]
+    )
+
+    def number(self):
+        return self.user
